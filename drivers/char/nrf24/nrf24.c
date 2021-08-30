83768b00e01f93743d3efd98c2a34aa59196fb9b
horvath0balint <horvath0balint@gmail.com>
2021-08-30 16:53:00 +0200

    Refactor tabs

--- drivers/char/nrf24/nrf24.c
+++ drivers/char/nrf24/nrf24.c
@@ -35,57 +35,57 @@ struct ADDRESS_T {
 
 
 // GPIO pin settings 
-#define NRF24_GPIO_CE 								16								// GPIO16
-#define NRF24_GPIO_SCLK								20								// GPIO20
-#define NRF24_GPIO_MOSI								21								// GPIO21
-#define NRF24_GPIO_CSN								26								// GPIO26
-#define NRF24_GPIO_IRQ	 							13								// GPIO13
-#define NRF24_GPIO_MISO								19								// GPIO19
-#define NRF24_SPI_HALF_CLK						100								// SCLK half period ns
+#define NRF24_GPIO_CE                 16                // GPIO16
+#define NRF24_GPIO_SCLK               20                // GPIO20
+#define NRF24_GPIO_MOSI               21                // GPIO21
+#define NRF24_GPIO_CSN                26                // GPIO26
+#define NRF24_GPIO_IRQ                13                // GPIO13
+#define NRF24_GPIO_MISO               19                // GPIO19
+#define NRF24_SPI_HALF_CLK            100               // SCLK half period ns
 
 // nRF24 commands
-#define NRF24_CMD_R_REGISTER 					0x00
-#define NRF24_CMD_W_REGISTER 					0x20
-#define NRF24_CMD_R_RX_PAYLOAD				0x61
-#define NRF24_CMD_W_TX_PAYLOAD				0xa0
-#define NRF24_CMD_FLUSH_TX 						0xe1
-#define NRF24_CMD_FLUSH_RX 						0xe2
-#define NRF24_CMD_REUSE_TX_PL 				0xe3
-#define NRF24_CMD_ACTIVATE						0x50
-#define NRF24_CMD_R_RX_PL_WID 				0x60
-#define NRF24_CMD_W_ACK_PAYLOAD 			0xa8
-#define NRF24_CMD_W_TX_PAYLOAD_NO_ACK	0xb0
-#define NRF24_CMD_NOP 								0xff
+#define NRF24_CMD_R_REGISTER          0x00
+#define NRF24_CMD_W_REGISTER          0x20
+#define NRF24_CMD_R_RX_PAYLOAD        0x61
+#define NRF24_CMD_W_TX_PAYLOAD        0xa0
+#define NRF24_CMD_FLUSH_TX            0xe1
+#define NRF24_CMD_FLUSH_RX            0xe2
+#define NRF24_CMD_REUSE_TX_PL         0xe3
+#define NRF24_CMD_ACTIVATE            0x50
+#define NRF24_CMD_R_RX_PL_WID         0x60
+#define NRF24_CMD_W_ACK_PAYLOAD       0xa8
+#define NRF24_CMD_W_TX_PAYLOAD_NO_ACK 0xb0
+#define NRF24_CMD_NOP                 0xff
 
 // nRF24 registers
-#define NRF24_REG_CONFIG							0x00
-#define NRF24_REG_EN_AA								0x01
-#define NRF24_REG_EN_RXADDR						0x02
-#define NRF24_REG_SETUP_AW						0x03
-#define NRF24_REG_SETUP_RETR					0x04
-#define NRF24_REG_RF_CH								0x05
-#define NRF24_REG_RF_SETUP						0x06
-#define NRF24_REG_STATUS							0x07
-#define NRF24_REG_OBSERVE_TX					0x08
-#define NRF24_REG_CD									0x09
-#define NRF24_REG_RX_ADDR_P0					0x0a
-#define NRF24_REG_RX_ADDR_P1					0x0b
-#define NRF24_REG_RX_ADDR_P2					0x0c
-#define NRF24_REG_RX_ADDR_P3					0x0d
-#define NRF24_REG_RX_ADDR_P4					0x0e
-#define NRF24_REG_RX_ADDR_P5					0x0f
-#define NRF24_REG_TX_ADDR							0x10
-#define NRF24_REG_RX_PW_P0						0x11
-#define NRF24_REG_RX_PW_P1						0x12
-#define NRF24_REG_RX_PW_P2						0x13
-#define NRF24_REG_RX_PW_P3						0x14
-#define NRF24_REG_RX_PW_P4						0x15
-#define NRF24_REG_RX_PW_P5						0x16
-#define NRF24_REG_FIFO_STATUS					0x17
-#define NRF24_REG_DYNPD								0x1c
-#define NRF24_REG_FEATURE							0x1d
+#define NRF24_REG_CONFIG              0x00
+#define NRF24_REG_EN_AA               0x01
+#define NRF24_REG_EN_RXADDR           0x02
+#define NRF24_REG_SETUP_AW            0x03
+#define NRF24_REG_SETUP_RETR          0x04
+#define NRF24_REG_RF_CH               0x05
+#define NRF24_REG_RF_SETUP            0x06
+#define NRF24_REG_STATUS              0x07
+#define NRF24_REG_OBSERVE_TX          0x08
+#define NRF24_REG_CD                  0x09
+#define NRF24_REG_RX_ADDR_P0          0x0a
+#define NRF24_REG_RX_ADDR_P1          0x0b
+#define NRF24_REG_RX_ADDR_P2          0x0c
+#define NRF24_REG_RX_ADDR_P3          0x0d
+#define NRF24_REG_RX_ADDR_P4          0x0e
+#define NRF24_REG_RX_ADDR_P5          0x0f
+#define NRF24_REG_TX_ADDR             0x10
+#define NRF24_REG_RX_PW_P0            0x11
+#define NRF24_REG_RX_PW_P1            0x12
+#define NRF24_REG_RX_PW_P2            0x13
+#define NRF24_REG_RX_PW_P3            0x14
+#define NRF24_REG_RX_PW_P4            0x15
+#define NRF24_REG_RX_PW_P5            0x16
+#define NRF24_REG_FIFO_STATUS         0x17
+#define NRF24_REG_DYNPD               0x1c
+#define NRF24_REG_FEATURE             0x1d
 
-#define NRF24_REG_STATUS_DEFAULT			0x0e
+#define NRF24_REG_STATUS_DEFAULT      0x0e
 
 typedef struct {
 	u8 id;
